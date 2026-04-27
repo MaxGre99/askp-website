5ce7eb3b888acad2a2701661acdea42dbbc26fcd
Maxim Grebenschikov <maksonster99@gmail.com>
2026-04-27 23:01:29 +0600

    feat: added different favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -68,9 +68,12 @@ export const metadata: Metadata = {
 	},
 
 	icons: {
-		icon: '/favicon.png',
-		shortcut: '/favicon.png',
-		apple: '/favicon.png',
+		icon: [
+			{ url: '/favicon.ico' },
+			{ url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+			{ url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+		],
+		apple: [{ url: '/apple-touch-icon.png', sizes: '180x180' }],
 	},
 
 	robots: {
